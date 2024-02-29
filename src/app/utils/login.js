eb3f671d97d8ff84dc2588fbbfb47d12e016ea87
Anh Khoa <mr.mrkhoacoding@gmail.com>
2024-02-29 17:21:46 +0700

    tetststsety

--- src/app/utils/login.js
+++ src/app/utils/login.js
@@ -3,7 +3,7 @@ import { SERVER_URL, accessLinks, redirectLinks } from '../configs/index';
 export const handleGetRedirect = async (
 	provider
 ) => {
-	const res = await fetch(SERVER_URL + redirectLinks[provider]);
+	const res = await fetch(`${SERVER_URL}${redirectLinks[provider]}`);
 	if (res.ok) {
 		const { data } = await res.json();
 		const { urlRedirect } = data.result;
@@ -17,9 +17,7 @@ export const handleCallback = async (
 	thisUrl
 ) => {
 	const queryParams = thisUrl.slice(thisUrl.indexOf('?') + 1);
-	const res = await fetch(
-		SERVER_URL + accessLinks[provider] + '?' + queryParams
-	);
+	const res = await fetch(`${SERVER_URL}${accessLinks[provider]}?${queryParams}`);
 	if (res.ok) {
 		const { data: { accessToken } } = await res.json();
 		return accessToken;
